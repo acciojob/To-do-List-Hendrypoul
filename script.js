d79cd660780b2945f04e1df4e233992625dbc8c6
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-02-05 00:30:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,34 +9,3 @@ if(data.value.length !==0){
 	list.appendChild(li);
 }
 }
-
-// list.addEventListener('click', function(ev) {
-//   if (ev.target.tagName === 'LI') {
-//     ev.target.classList.toggle('checked');
-//   }
-// }, false);
-// function newElement() {
-//   var li = document.createElement("li");
-//   var newTodoInput = document.getElementById("newTodoInput").value;
-//   var t = document.createTextNode(newTodoInput);
-//   li.appendChild(t);
-//   if (inputValue === '') {
-//     alert("You must write something!");
-//   } else {
-//     document.getElementById("todoList").appendChild(li);
-//   }
-//   document.getElementById("newTodoInput").value = "";
-
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   li.appendChild(span);
-
-//   for (i = 0; i < close.length; i++) {
-//     close[i].onclick = function() {
-//       var div = this.parentElement;
-//       div.style.display = "none";
-//     }
-//   }
-// }
