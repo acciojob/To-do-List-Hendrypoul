7a3ddf1e0b40fb62d0bdf3560017767bfc43afb7
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-02-04 23:44:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-var list = document.querySelector('ul');
+var list = document.querySelector('ol');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
-
-// Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
+  var inputValue = document.getElementById("newTodoInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("todoList").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
+  document.getElementById("newTodoInput").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
