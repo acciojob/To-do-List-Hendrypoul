58f8ab23447d6a88c50267998c7720c1b963c31e
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-02-05 11:34:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var list = document.querySelector('ol');
 var data=document.querySelector('input');
 
 function addItem(){
-if(data.value.length !==0){
+if(data.value.trim().length !==0){
 	var li = document.createElement("li");
 	const textnode=document.createTextNode(data.value);
 	li.appendChild(textnode);
 	list.appendChild(li);
+	data.value="";
 }
 }
